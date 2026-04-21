06bd5dae62ecc61c49f8de5ffba68397bce720b7
Alexander Chai <achai38@bloomberg.net>
2026-04-20 21:23:25 -0400

    add initial stuff for fold equality

--- Origami/FOLD/intersectionsCoextensivity.lean
+++ Origami/FOLD/intersectionsCoextensivity.lean
@@ -1,11 +1,22 @@
 import Origami.FOLD.Properties
 import Origami.FOLD.WellFormed
 
+def pointOnEdge (p : List ℝ) (u : List ℝ) (v : List ℝ) : Prop :=
+  ∃ t : ℝ, p = List.zipWith (fun a b => (1 - t) * a + t * b) pu pv
+
 def pointOnFold (F : Fold) (p : List ℝ) : Prop :=
   ∃ e ∈ F.edges,
     let pu := (F.vertices.get! e.u).coords
     let pv := (F.vertices.get! e.v).coords
-    ∃ t : ℝ, p = List.zipWith (fun a b => (1 - t) * a + t * b) pu pv
+    pointOnEdge p pu pv
 
 def pointsOnFold (F : Fold) (ps : List (List ℝ)) : Prop :=
     ∀ p ∈ ps, pointOnFold F p
+
+def foldsEqualUpToDirection (F1 : Fold) (F1 : Fold) : Prop :=
+  ∃ e1 ∈ F1.edges, ∃ e2 ∈ F1.edges,
+    let pu1 := (F1.vertices.get! e.u).coords
+    let pv1 := (F1.vertices.get! e.v).coords
+    let pu2 := (F2.vertices.get! e.u).coords
+    let pv2 := (F2.vertices.get! e.v).coords
+    pointOnEdge pu2 pu1 pv1 and pointOnEdge pv2 pu1 pv1
