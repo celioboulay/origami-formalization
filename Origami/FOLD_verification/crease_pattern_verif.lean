18f38d829e59f81e1118a1095318da44917f3c32
Celio <celio.boulay@outlook.fr>
2026-05-08 16:10:34 -0400

    added comments

--- Origami/FOLD_verification/crease_pattern_verif.lean
+++ Origami/FOLD_verification/crease_pattern_verif.lean
@@ -6,18 +6,16 @@ structure Point where
   x : ℚ
   y : ℚ
 
-abbrev Vertex := Point -- the vertex of the pattern
+abbrev Vertex := Point -- a Vertex is a point on the inside of the unit square
 
 inductive Label
   | V | M
 deriving DecidableEq
 
-
 structure Ray where
   u : Point
   v : Point
   c : Label
-
 
 structure CreasePattern where
   γ : Set Vertex
@@ -25,10 +23,12 @@ structure CreasePattern where
   E : Set Ray
 
 
-/- assume edges are unit square -/
+/- Maekawa's theorem states at every vertex,
+the numbers of valley and mountain folds always differ by two in either direction.
+https://en.wikipedia.org/wiki/Maekawa%27s_theorem -/
 
--- the numbers of valley and mountain folds always differ by two
-
+/- Maekawa_condition P means that P verifies the condition
+Note that this condition is necessary for the fold to be flat-foldable but no sufficient -/
 def Maekawa_condition (P : CreasePattern) : Prop :=
   ∀ v ∈ P.γ,
     let M := {e ∈ P.E | (e.u = v ∨ e.v = v) ∧ e.c = Label.M}
@@ -38,18 +38,18 @@ def Maekawa_condition (P : CreasePattern) : Prop :=
     Int.natAbs (n_M - n_V) = 2
 
 
+/- *TODO* add corollary (total number of folds at each vertex must be an even number) -/
 
 
--- For Kawasaki
-/- A one-vertex crease pattern consists of a set of rays or creases
-drawn on a flat sheet of paper, all emanating from the same
-point interior to the sheet
--/
+/- Kawasaki's theorem.
+A one-vertex crease pattern consists of a set of rays or creases drawn on a
+flat sheet of paper, all emanating from the same point interior to the sheet. -/
 
-/- number of creases must be even
+/- The number of creases must be even (get from Maekawa maybe?)
 Then Kawasaki's theorem states that the crease pattern may be folded flat
-iff : α1 − α2 + α3 − ⋯ + α(2n-1) − α2n = 0 -/
+iff : α1 − α2 + α3 − ⋯ + α(2n-1) − α2n = 0
+https://en.wikipedia.org/wiki/Kawasaki%27s_theorem -/
 
-def Kawasaki_condition {P : CreasePattern}
+def Kawasaki_condition {P : CreasePattern} /- *TODO* -/
   (hγ : P.γ.ncard = 1) (h_even : Even P.V.ncard) :=
     P = P
